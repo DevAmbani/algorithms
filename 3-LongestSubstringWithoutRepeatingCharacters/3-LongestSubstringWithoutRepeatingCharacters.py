355c0a2372c1fc88a58a89c9b05afd833fab40ad
DevAmbani <147029224+DevAmbani@users.noreply.github.com>
2025-10-31 11:47:13 -0500

    File updated at 10/31/2025, 11:47:13 AM

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
@@ -1,17 +1,27 @@
-# Last updated: 10/31/2025, 11:24:13 AM
+# Last updated: 10/31/2025, 11:47:13 AM
 class Solution:
-    def characterReplacement(self, s: str, k: int) -> int:
-        count = {}
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        
+        s1_count = {}
+        s2_count = {}
+        for k in range(len(s1)):
+            s1_count[s1[k]] = 1 + s1_count.get(s1[k], 0)
+
+        window = len(s1)
         l = 0
-        res = 0
+        for r in range(len(s2)):
+            s2_count[s2[r]] = 1 + s2_count.get(s2[r], 0)
 
-        for r in range(len(s)):
-            count[s[r]] = 1 + count.get(s[r], 0)
+            if r - l + 1 > window:
+                s2_count[s2[l]] -= 1
 
-            if (r-l+1) - max(count.values()) > k:
-                count[s[l]] = count[s[l]] - 1
+                if s2_count[s2[l]] == 0:
+                    del s2_count[s2[l]]
+                    
                 l += 1
-
-            res = max(res, r-l+1)
             
-        return res
+            if s1_count == s2_count:
+                return True
+        return False
