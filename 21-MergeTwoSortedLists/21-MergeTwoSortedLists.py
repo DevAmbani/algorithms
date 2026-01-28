252c60d3ff8a6c38c3d3b286a57ce773c5a10c66
DevAmbani <147029224+DevAmbani@users.noreply.github.com>
2026-01-28 12:57:39 -0600

    File updated at 1/28/2026, 12:57:38 PM

--- 21-MergeTwoSortedLists/21-MergeTwoSortedLists.py
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.py
@@ -1,4 +1,4 @@
-# Last updated: 1/28/2026, 12:46:13 PM
+# Last updated: 1/28/2026, 12:57:38 PM
 1# Definition for singly-linked list.
 2# class ListNode:
 3#     def __init__(self, x):
@@ -7,11 +7,13 @@
 6
 7class Solution:
 8    def hasCycle(self, head: Optional[ListNode]) -> bool:
-9        tracker = set()
-10        curr = head
-11        while curr:
-12            if curr in tracker:
-13                return True
-14            tracker.add(curr)
-15            curr = curr.next
-16        return False
+9        if head is None:
+10            return False
+11        p1 = head
+12        p2 = head.next
+13        while p2 and p2.next:
+14            if p1 == p2:
+15                return True
+16            p1 = p1.next
+17            p2 = p2.next.next
+18        return False
