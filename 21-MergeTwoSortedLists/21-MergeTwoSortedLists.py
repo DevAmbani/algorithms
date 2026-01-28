4e166be4e0fa0c137131d4c321bf83577fe24561
DevAmbani <147029224+DevAmbani@users.noreply.github.com>
2026-01-28 13:16:51 -0600

    File updated at 1/28/2026, 1:16:51 PM

--- 21-MergeTwoSortedLists/21-MergeTwoSortedLists.py
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.py
@@ -1,4 +1,4 @@
-# Last updated: 1/28/2026, 12:57:38 PM
+# Last updated: 1/28/2026, 1:16:51 PM
 1# Definition for singly-linked list.
 2# class ListNode:
 3#     def __init__(self, x):
@@ -12,7 +12,7 @@
 11        p1 = head
 12        p2 = head.next
 13        while p2 and p2.next:
-14            if p1 == p2:
+14            if p1 is p2:
 15                return True
 16            p1 = p1.next
 17            p2 = p2.next.next
