f77f13dfce7010f51a54fbcf7e7267f4adf6696f
DevAmbani <147029224+DevAmbani@users.noreply.github.com>
2026-02-03 14:46:49 -0600

    File updated at 2/3/2026, 2:46:49 PM

--- 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.py
+++ 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.py
@@ -1,8 +1,22 @@
-# Last updated: 2/3/2026, 9:33:32 AM
+# Last updated: 2/3/2026, 2:46:49 PM
 1class Solution:
 2    def findDuplicate(self, nums: List[int]) -> int:
-3        seen = set()
-4        for i in nums:
-5            if i in seen:
-6                return i
-7            seen.add(i)
+3        slow, fast = 0, 0
+4
+5        while True:
+6            slow = nums[slow]
+7            fast = nums[fast]
+8            fast = nums[fast]
+9
+10            if slow == fast:
+11                break
+12        
+13        start = 0
+14        while True:
+15            start = nums[start]
+16            slow = nums[slow]
+17
+18            if start == slow:
+19                break
+20        
+21        return slow
