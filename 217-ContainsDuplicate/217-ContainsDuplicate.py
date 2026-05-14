18fb01121bb536e20da6f847da79741529ce7eac
DevAmbani <147029224+DevAmbani@users.noreply.github.com>
2026-05-13 23:04:19 -0500

    File updated at 5/13/2026, 11:04:19 PM

--- 217-ContainsDuplicate/217-ContainsDuplicate.py
+++ 217-ContainsDuplicate/217-ContainsDuplicate.py
@@ -1,9 +1,18 @@
-# Last updated: 2/7/2026, 11:21:58 PM
+# Last updated: 5/13/2026, 11:04:19 PM
 1class Solution:
-2    def containsDuplicate(self, nums: List[int]) -> bool:
-3        seen = set()
-4        for num in nums:
-5            if num in seen:
-6                return True
-7            seen.add(num)
-8        return False
+2    def isAnagram(self, s: str, t: str) -> bool:
+3        if len(s) != len(t):
+4            return False
+5
+6        counts = defaultdict(int)
+7        countt = defaultdict(int)
+8
+9        for i in range(len(s)):
+10            counts[s[i]] += 1
+11            countt[t[i]] += 1
+12        
+13        if counts == countt:
+14            return True
+15        else:
+16            return False
+17            
