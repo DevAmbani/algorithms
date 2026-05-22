af12b13ca555806345819029a1294bd585bd74b8
DevAmbani <147029224+DevAmbani@users.noreply.github.com>
2026-05-22 11:43:20 -0500

    File updated at 5/22/2026, 11:43:20 AM

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.py
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.py
@@ -1,16 +1,29 @@
-# Last updated: 5/22/2026, 11:11:16 AM
+# Last updated: 5/22/2026, 11:43:20 AM
 1class Solution:
-2    def maxArea(self, height: List[int]) -> int:
-3        ans = 0
-4        l ,r = 0, len(height)-1
-5
-6        while l < r:
-7            area = (r-l) * (min(height[l], height[r]))
-8            ans = max(ans, area)
-9            if height[l] >= height[r]:
-10                r -= 1
-11            else:
-12                l += 1
-13
-14        return ans
-15
+2    def isValid(self, s: str) -> bool:
+3        adder = []
+4
+5        for c in s:
+6            if c == "(" or c == "{" or c == "[":
+7                adder.append(c)
+8            elif c == ")":
+9                if not adder or adder[-1] != "(":
+10                    return False
+11                else:
+12                    adder.pop()
+13            elif c == "}": 
+14                if not adder or adder[-1] != "{":
+15                    return False
+16                else:
+17                    adder.pop()
+18            elif c == "]": 
+19                if not adder or adder[-1] != "[":
+20                    return False
+21                else:
+22                    adder.pop()
+23        
+24        if len(adder) == 0:
+25            return True
+26        else:
+27            return False
+28
