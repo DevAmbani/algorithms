9f585fc71fd0241a6ad409aecefde88dc64a4b23
DevAmbani <147029224+DevAmbani@users.noreply.github.com>
2026-05-23 23:29:31 -0500

    File updated at 5/23/2026, 11:29:31 PM

--- 20-ValidParentheses/20-ValidParentheses.py
+++ 20-ValidParentheses/20-ValidParentheses.py
@@ -1,10 +1,16 @@
-# Last updated: 5/23/2026, 10:11:47 PM
+# Last updated: 5/23/2026, 11:29:31 PM
 1class Solution:
 2    def findMin(self, nums: List[int]) -> int:
 3        ans = nums[0]
-4
-5        for n in nums:
-6            ans = min(ans, n)
-7
-8        return ans
-9        
+4        l = 0
+5        r = len(nums)-1
+6
+7        while l < r:
+8            mid = (l+r) // 2
+9            if nums[mid] > nums[r]:
+10                l = mid+1
+11            elif nums[mid] < nums[r]:
+12                r = mid
+13        
+14        return nums[l]
+15        
