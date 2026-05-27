bc7f0889246e98e6a74b6803030c548c40024dbd
DevAmbani <147029224+DevAmbani@users.noreply.github.com>
2026-05-27 18:57:13 -0500

    File updated at 5/27/2026, 6:57:12 PM

--- 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.py
+++ 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.py
@@ -1,22 +1,13 @@
-# Last updated: 5/26/2026, 12:27:27 PM
+# Last updated: 5/27/2026, 6:57:12 PM
 1class Solution:
-2    def search(self, nums: List[int], target: int) -> int:
+2    def maxProfit(self, prices: List[int]) -> int:
 3        l = 0
-4        r = len(nums) - 1
-5        
-6        while l <= r:
-7            mid = (l+r) // 2
-8            if nums[mid] == target:
-9                return mid
-10            elif nums[l] > nums[mid]:
-11                if target <= nums[r] and target > nums[mid]:
-12                    l = mid+1
-13                else:
-14                    r = mid-1
-15            else:
-16                if target >= nums[l] and target < nums[mid]:
-17                    r = mid-1
-18                else:
-19                    l = mid+1
-20        
-21        return -1
+4        ans = 0
+5        lowest = prices[0]
+6
+7        for r in range(len(prices)):
+8            lowest = min(lowest, prices[r])
+9            compare = prices[r] - lowest
+10            ans = max(ans, compare)
+11
+12        return ans
