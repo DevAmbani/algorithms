1dd81fe6ce60263a494846ebc0be8f9524e1e16e
DevAmbani <147029224+DevAmbani@users.noreply.github.com>
2026-06-24 08:54:25 -0500

    File updated at 6/24/2026, 8:54:25 AM

--- 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.py
+++ 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.py
@@ -1,28 +1,24 @@
-# Last updated: 6/24/2026, 8:28:20 AM
-1from collections import deque
-2# Definition for a binary tree node.
-3# class TreeNode:
-4#     def __init__(self, val=0, left=None, right=None):
-5#         self.val = val
-6#         self.left = left
-7#         self.right = right
-8class Solution:
-9    def isValidBST(self, root: Optional[TreeNode]) -> bool:
-10        if not root:
-11            return True
-12        
-13        values = []
-14        def order(node):   
-15            if node.left:
-16                order(node.left)
-17            values.append(node.val)
-18            if node.right:
-19                order(node.right)
-20        
-21        order(root)
-22        print(values)
-23        for i in range(len(values)-1):
-24            if (values[i] >= values[i+1]):
-25                return False
-26        
-27        return True
+# Last updated: 6/24/2026, 8:54:25 AM
+1# Definition for a binary tree node.
+2# class TreeNode:
+3#     def __init__(self, val=0, left=None, right=None):
+4#         self.val = val
+5#         self.left = left
+6#         self.right = right
+7class Solution:
+8    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+9        values = []
+10
+11        def order(node):
+12            if not node:
+13                return None
+14            order(node.left)
+15            values.append(node.val)
+16            order(node.right)
+17        
+18        order(root)
+19        print(values)
+20
+21        for i in range(len(values)):
+22            if i+1 == k:
+23                return values[i]
