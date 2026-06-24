82be3c27349fc7dc0f184fac066c70142190a2ce
DevAmbani <147029224+DevAmbani@users.noreply.github.com>
2026-06-24 08:28:20 -0500

    File updated at 6/24/2026, 8:28:20 AM

--- 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.py
+++ 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.py
@@ -1,37 +1,28 @@
-# Last updated: 6/23/2026, 6:17:20 PM
+# Last updated: 6/24/2026, 8:28:20 AM
 1from collections import deque
-2
-3# Definition for a binary tree node.
-4# class TreeNode:
-5#     def __init__(self, val=0, left=None, right=None):
-6#         self.val = val
-7#         self.left = left
-8#         self.right = right
-9class Solution:
-10    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-11        if not root:
-12            return []
-13        
-14        res = []
-15        queue = collections.deque()
-16        queue.append(root)
-17
-18        while queue:
-19            length = len(queue)
-20            level = []
-21
-22            for q in range(length):
-23                node = queue.popleft()
-24                level.append(node.val)
-25
-26                if node.left:
-27                    queue.append(node.left)
-28                if node.right:
-29                    queue.append(node.right)
-30            
-31            if level:
-32                res.append(level)
-33        
-34        return res
-35
-36        
+2# Definition for a binary tree node.
+3# class TreeNode:
+4#     def __init__(self, val=0, left=None, right=None):
+5#         self.val = val
+6#         self.left = left
+7#         self.right = right
+8class Solution:
+9    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+10        if not root:
+11            return True
+12        
+13        values = []
+14        def order(node):   
+15            if node.left:
+16                order(node.left)
+17            values.append(node.val)
+18            if node.right:
+19                order(node.right)
+20        
+21        order(root)
+22        print(values)
+23        for i in range(len(values)-1):
+24            if (values[i] >= values[i+1]):
+25                return False
+26        
+27        return True
