1c648af646268c88463ee4feeafe941036ce0dd9
DevAmbani <147029224+DevAmbani@users.noreply.github.com>
2026-06-24 10:36:25 -0500

    File updated at 6/24/2026, 10:36:24 AM

--- 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.py
+++ 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.py
@@ -1,4 +1,4 @@
-# Last updated: 6/24/2026, 8:54:45 AM
+# Last updated: 6/24/2026, 10:36:24 AM
 1# Definition for a binary tree node.
 2# class TreeNode:
 3#     def __init__(self, val=0, left=None, right=None):
@@ -6,17 +6,15 @@
 5#         self.left = left
 6#         self.right = right
 7class Solution:
-8    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-9        values = []
-10
-11        def order(node):
-12            if not node:
-13                return None
-14            order(node.left)
-15            values.append(node.val)
-16            order(node.right)
-17        
-18        order(root)
-19        print(values)
-20
-21        return values[k-1]
+8    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+9        if not preorder or not inorder:
+10            return None
+11        
+12        root = TreeNode(preorder[0])
+13        mid = inorder.index(preorder[0])
+14
+15        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
+16        root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
+17
+18        return root
+19        
