c008695cefa7f452bd08fb61469543de552cb2c8
DevAmbani <147029224+DevAmbani@users.noreply.github.com>
2026-06-24 19:33:37 -0500

    File updated at 6/24/2026, 7:33:36 PM

--- 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.py
+++ 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.py
@@ -1,22 +1,25 @@
-# Last updated: 6/24/2026, 7:19:12 PM
+# Last updated: 6/24/2026, 7:33:36 PM
 1import heapq
-2
-3class Solution:
-4    def lastStoneWeight(self, stones: List[int]) -> int:
-5        heap = []
-6
-7        for i in range(len(stones)):
-8            heapq.heappush(heap, -stones[i])
-9        
-10        while len(heap) > 1:
-11            first = heapq.heappop(heap)
-12            second = heapq.heappop(heap)
-13
-14            if first != second:
-15                heapq.heappush(heap, first-second)
-16        
-17        if len(heap) == 0:
-18            return 0
-19        
-20        return -heapq.heappop(heap)
-21        
+2class MedianFinder:
+3    def __init__(self):
+4        self.arr = []
+5
+6    def addNum(self, num: int) -> None:
+7        self.arr.append(num)
+8
+9    def findMedian(self) -> float:
+10        self.arr.sort()
+11        length = len(self.arr)
+12        
+13        if length % 2 != 0:
+14            return float(self.arr[length // 2])
+15        else:
+16            mid = length // 2
+17            return float((self.arr[mid] + self.arr[mid - 1]) / 2)
+18        
+19
+20
+21# Your MedianFinder object will be instantiated and called as such:
+22# obj = MedianFinder()
+23# obj.addNum(num)
+24# param_2 = obj.findMedian()
