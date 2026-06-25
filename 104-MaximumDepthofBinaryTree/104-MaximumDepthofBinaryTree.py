30e202aa76d6b39208f22a3f6f2559f20aeb6057
DevAmbani <147029224+DevAmbani@users.noreply.github.com>
2026-06-25 15:44:40 -0500

    File updated at 6/25/2026, 3:44:40 PM

--- 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.py
+++ 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.py
@@ -1,27 +1,38 @@
-# Last updated: 6/25/2026, 10:53:46 AM
-1class Solution:
-2    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-3        rows = len(grid)
-4        elements = len(grid[0])
-5        maxarea = 0
-6
-7        def dfs(row, element):
-8            if row < 0 or row >= rows:
-9                return 0
-10            if element < 0 or element >= elements:
-11                return 0
-12            if grid[row][element] == 0:
-13                return 0
-14            
-15            grid[row][element] = 0
-16            
-17            return 1 + dfs(row+1, element) + dfs(row-1, element) + dfs(row, element+1) + dfs(row, element-1)
-18
-19        for row in range(len(grid)):
-20            for element in range(len(grid[0])):
-21                if grid[row][element] == 1:
-22                    area = dfs(row, element)
-23                    maxarea = max(maxarea, area)
-24        
-25        return maxarea
-26        
+# Last updated: 6/25/2026, 3:44:40 PM
+1from collections import deque
+2
+3class Solution:
+4    def orangesRotting(self, grid: List[List[int]]) -> int:
+5        rows = len(grid)
+6        cols = len(grid[0])
+7        queue = deque()
+8        fresh = 0
+9        
+10        for row in range(rows):
+11            for col in range(cols):
+12                if grid[row][col] == 2:
+13                    queue.append([row, col])
+14                if grid[row][col] == 1:
+15                    fresh += 1
+16        
+17        coord = [[0,1], [0,-1], [1,0], [-1,0]]
+18        minutes = 0
+19                
+20        while queue and fresh > 0:
+21            minutes += 1
+22
+23            for _ in range(len(queue)):
+24                r, c = queue.popleft()
+25
+26                for dr, dc in coord:
+27
+28                    nr, nc = r+dr, c+dc
+29                    if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1:
+30                        grid[nr][nc] = 2
+31                        fresh -= 1
+32                        queue.append([nr, nc])
+33        
+34        if fresh == 0:
+35            return minutes
+36        return -1
+37                
