636fc7d3eec7de2a4025f84700b5a1193684d779
DevAmbani <147029224+DevAmbani@users.noreply.github.com>
2026-06-28 08:47:55 -0500

    File updated at 6/28/2026, 8:47:54 AM

--- 417-PacificAtlanticWaterFlow/417-PacificAtlanticWaterFlow.py
+++ 417-PacificAtlanticWaterFlow/417-PacificAtlanticWaterFlow.py
@@ -1,41 +1,35 @@
-# Last updated: 6/25/2026, 5:57:32 PM
-1from collections import deque
-2
-3class Solution:
-4    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-5        rows = len(heights)
-6        cols = len(heights[0])
-7        poq = deque()
-8        aoq = deque()
-9
-10        def bfs(queue):
-11            directions = [[1,0],[-1,0],[0,1],[0,-1]]
-12            visited = set(queue)
-13            while queue:
-14                for _ in range(len(queue)):
-15                    r, c = queue.popleft()
-16                    for dr, dc in directions:
-17                        nr, nc = r+dr, c+dc
-18                        
-19                        if 0 <= nr < rows and 0 <= nc < cols and heights[r][c] <= heights[nr][nc] and (nr,nc) not in visited:
-20                            queue.append([nr, nc])
-21                            visited.add((nr,nc))
-22            return visited
-23
-24        for row in range(rows):
-25            for col in range(cols):
-26                if row == 0 or col == 0:
-27                    poq.append((row, col))
-28                if row == rows-1 or col == cols-1:
-29                    aoq.append((row, col))
-30        
-31        pacific = bfs(poq)
-32        atlantic = bfs(aoq)
-33        ans = []
-34
-35        for element in pacific:
-36            if element in atlantic:
-37                ans.append(element)
-38        
-39        return ans
-40        
+# Last updated: 6/28/2026, 8:47:54 AM
+1class Solution:
+2    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+3        parent = list(range(n))
+4        rank = [0] * n
+5        components = n
+6
+7        def find(node):
+8            if parent[node] == node:
+9                return node
+10            elif parent[node] != node:
+11                return find(parent[node])
+12        
+13        def union(x, y):
+14            px = find(x)
+15            py = find(y)
+16
+17            if px == py:
+18                return False
+19
+20            if rank[px] > rank[py]:
+21                parent[py] = px
+22            elif rank[py] > rank[px]:
+23                parent[px] = py
+24            else:
+25                parent[py] = px
+26                rank[px] += 1
+27
+28            return True
+29        
+30        for x,y in edges:
+31            if union(x,y):
+32                components -= 1
+33        
+34        return components
