f9ad403d83561e4761fa94060c55b918a7e7dad6
DevAmbani <147029224+DevAmbani@users.noreply.github.com>
2026-06-28 14:37:33 -0500

    File updated at 6/28/2026, 2:37:33 PM

--- 417-PacificAtlanticWaterFlow/417-PacificAtlanticWaterFlow.py
+++ 417-PacificAtlanticWaterFlow/417-PacificAtlanticWaterFlow.py
@@ -1,61 +1,44 @@
-# Last updated: 6/28/2026, 1:12:23 PM
-1from collections import defaultdict
-2
-3class Solution:
-4    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
-5        acc = defaultdict(list)
-6        parent = {}
-7        rank = {}
-8
-9        def find(node):
-10            if node == parent[node]:
-11                return node
-12            elif node != parent[node]:
-13                return find(parent[node])
-14            
-15        def union(x,y):
-16            px = find(x)
-17            py = find(y)
-18
-19            if px == py:
-20                return False
-21            
-22            elif rank[px] > rank[py]:
-23                parent[py] = px
-24            elif rank[py] > rank[px]:
-25                parent[px] = py
-26            else:
-27                parent[py] = px
-28                rank[px] += 1
-29            return True
-30
-31        for account in accounts:
-32            for email in account[1:]:
-33                if email not in parent:
-34                    parent[email] = email
-35                    rank[email] = 0
-36        
-37        for account in accounts:
-38            anchor = account[1]
-39            for email in account[2:]:
-40                union(anchor, email)
-41        
-42        group = defaultdict(list)
-43        for account in accounts:
-44            for email in account[1:]:
-45                root = find(email)
-46                group[root].append(email)
-47        
-48        email_name = {}
-49        for account in accounts:
-50            name = account[0]
-51            for email in account[1:]:
-52                email_name[email] = name
-53        
-54        final = []
-55        for root, email in group.items():
-56            name = email_name[root]
-57            final.append([name] + sorted(set(email)))
-58        
-59        return final
-60
+# Last updated: 6/28/2026, 2:37:33 PM
+1class Solution:
+2    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+3        n = len(points)
+4        parent = list(range(n))
+5        rank = [0] * n
+6
+7        def find(node):
+8            if node == parent[node]:
+9                return node
+10            if node != parent[node]:
+11                return find(parent[node])
+12        
+13        def union(x,y):
+14            px = find(x)
+15            py = find(y)
+16
+17            if px == py:
+18                return False
+19            
+20            if rank[px] > rank[py]:
+21                parent[py] = px
+22            elif rank[py] > rank[px]:
+23                parent[px] = py
+24            else:
+25                parent[py] = px
+26                rank[px] += 1
+27            
+28            return True
+29        
+30        cost = []
+31        for i in range(n):
+32            for j in range(i+1, n):
+33                price = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])
+34                cost.append([price, i, j])
+35        
+36        cost.sort()
+37        ans = 0
+38
+39        for price, i, j in cost:
+40            if union(i, j):
+41                ans += price
+42        
+43        return ans
