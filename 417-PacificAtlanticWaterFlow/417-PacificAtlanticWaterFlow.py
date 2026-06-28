97fb437e614be39e93c1a009a9830d5ee5c60ac4
DevAmbani <147029224+DevAmbani@users.noreply.github.com>
2026-06-28 11:10:50 -0500

    File updated at 6/28/2026, 11:10:50 AM

--- 417-PacificAtlanticWaterFlow/417-PacificAtlanticWaterFlow.py
+++ 417-PacificAtlanticWaterFlow/417-PacificAtlanticWaterFlow.py
@@ -1,35 +1,37 @@
-# Last updated: 6/28/2026, 8:47:54 AM
+# Last updated: 6/28/2026, 11:10:50 AM
 1class Solution:
-2    def countComponents(self, n: int, edges: List[List[int]]) -> int:
-3        parent = list(range(n))
-4        rank = [0] * n
-5        components = n
-6
-7        def find(node):
-8            if parent[node] == node:
-9                return node
-10            elif parent[node] != node:
-11                return find(parent[node])
-12        
-13        def union(x, y):
-14            px = find(x)
-15            py = find(y)
-16
-17            if px == py:
-18                return False
-19
-20            if rank[px] > rank[py]:
-21                parent[py] = px
-22            elif rank[py] > rank[px]:
-23                parent[px] = py
-24            else:
-25                parent[py] = px
-26                rank[px] += 1
-27
-28            return True
-29        
-30        for x,y in edges:
-31            if union(x,y):
-32                components -= 1
-33        
-34        return components
+2    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+3        rank = [0] * n
+4        parent = list(range(n))
+5
+6        if n-1 != len(edges):
+7            return False
+8
+9        def find(node):
+10            if node == parent[node]:
+11                return node
+12            if node != parent[node]:
+13                return find(parent[node])
+14        
+15        def union(x, y):
+16            px = find(x)
+17            py = find(y)
+18
+19            if px == py:
+20                return False
+21            
+22            if rank[px] > rank[py]:
+23                parent[py] = px
+24            elif rank [py] > rank[px]:
+25                parent[px] = py
+26            else:
+27                parent[py] = px
+28                rank[px] += 1
+29            
+30            return True
+31        
+32        for x,y in edges:
+33             if union(x,y) is False:
+34                return False
+35        
+36        return True
