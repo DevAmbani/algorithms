99e722cbbe527f7a4436c57f871813f1f26a0f81
DevAmbani <147029224+DevAmbani@users.noreply.github.com>
2026-06-29 07:59:50 -0500

    File updated at 6/29/2026, 7:59:50 AM

--- 417-PacificAtlanticWaterFlow/417-PacificAtlanticWaterFlow.py
+++ 417-PacificAtlanticWaterFlow/417-PacificAtlanticWaterFlow.py
@@ -1,32 +1,25 @@
-# Last updated: 6/28/2026, 9:32:29 PM
-1import heapq
-2from collections import defaultdict
-3
-4class Solution:
-5    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-6        graph = defaultdict(list)
-7        dist = defaultdict(int)
-8        
-9        for u,v,w in times:
-10            graph[u].append([v,w])
+# Last updated: 6/29/2026, 7:59:50 AM
+1from collections import defaultdict
+2
+3class Solution:
+4    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+5        dist = defaultdict(int)
+6        
+7        for i in range(n):
+8            dist[i] = float('inf')
+9        
+10        dist[src] = 0
 11
-12        for i in (range(1, n+1)):
-13            dist[i] = float('inf')
-14        
-15        dist[k] = 0
-16
-17        heap = [(0,k)]
-18
-19        while heap:
-20            distance, node = heapq.heappop(heap)
-21            if distance > dist[node]:
-22                continue
-23            
-24            for neighbor, weight in graph[node]:
-25                new_distance = dist[node] + weight
-26                if new_distance < dist[neighbor]:
-27                    dist[neighbor] = new_distance
-28                    heapq.heappush(heap, (new_distance, neighbor))
-29        
-30        max_dist = max(dist.values())
-31        return max_dist if max_dist < float('inf') else -1
+12        for i in range(k+1):
+13
+14            temp = dist.copy()
+15            for source, dest, cost in flights:
+16                new_dist = dist[source] + cost
+17                if new_dist < temp[dest]:
+18                    temp[dest] = new_dist
+19            dist = temp
+20        
+21        if dist[dst] == float('inf'):
+22            return -1
+23        
+24        return dist[dst]
