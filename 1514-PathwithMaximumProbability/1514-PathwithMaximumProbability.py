ba36ec7e098b9df5c723729f53e86fe19cd989b3
DevAmbani <147029224+DevAmbani@users.noreply.github.com>
2026-06-29 10:14:40 -0500

    File updated at 6/29/2026, 10:14:40 AM

--- 1514-PathwithMaximumProbability/1514-PathwithMaximumProbability.py
+++ 1514-PathwithMaximumProbability/1514-PathwithMaximumProbability.py
@@ -1,35 +1,39 @@
-# Last updated: 6/29/2026, 8:33:37 AM
-1from collections import defaultdict
-2import heapq
-3
-4class Solution:
-5    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
-6        cost_graph = defaultdict(list)
-7        dist = defaultdict(int)
-8        count = 0
-9
-10        for u,v in edges:
-11            cost_graph[u].append([v, succProb[count]])
-12            cost_graph[v].append([u, succProb[count]])
-13            dist[count] = 0
-14            count += 1
-15        
-16        dist[start_node] = 1
-17
-18        neighbor_heap = [(-1, start_node)]
-19
-20        while neighbor_heap:
-21            distance, node = heapq.heappop(neighbor_heap)
-22            distance = -distance
-23
-24            if distance < dist[node]:
-25                continue
-26            
-27            for neighbor, cost in cost_graph[node]:
-28                new_dist = dist[node] * cost
-29                if new_dist > dist[neighbor]:
-30                    dist[neighbor] = new_dist
-31                    heapq.heappush(neighbor_heap, (-new_dist, neighbor))
-32            
-33        return dist[end_node]
-34
+# Last updated: 6/29/2026, 10:14:40 AM
+1class Solution:
+2    def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
+3        matrix = []
+4
+5        for rows in range(n):
+6            row = []
+7            for cols in range(n):
+8                if rows == cols:
+9                    row.append(0)
+10                else:
+11                    row.append(float('inf'))
+12            matrix.append(row)
+13
+14        for src, dest, cost in edges:
+15            matrix[src][dest] = cost
+16            matrix[dest][src] = cost
+17        
+18        for k in range(n):
+19            for i in range(n):
+20                for j in range(n):
+21                    if matrix[i][k] + matrix[k][j] < matrix[i][j]:
+22                        matrix[i][j] = matrix[i][k] + matrix[k][j]
+23        
+24        ans = -1
+25        ans_count = float('inf')
+26        for row in range(n):
+27            count = 0
+28            for col in range(n):
+29                if matrix[row][col] <= distanceThreshold:
+30                    count += 1
+31            
+32            if count <= ans_count:
+33                ans_count = count
+34                ans = row
+35        
+36        return ans
+37
+38
