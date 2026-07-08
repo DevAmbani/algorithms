a0e2cfd262947ab1604e1724782425e29fff99ee
DevAmbani <147029224+DevAmbani@users.noreply.github.com>
2026-07-08 17:22:05 -0500

    File updated at 7/8/2026, 5:22:04 PM

--- 46-Permutations/46-Permutations.py
+++ 46-Permutations/46-Permutations.py
@@ -1,18 +1,25 @@
-# Last updated: 7/8/2026, 3:55:24 PM
+# Last updated: 7/8/2026, 5:22:04 PM
 1class Solution:
-2    def permute(self, nums: List[int]) -> List[List[int]]:
+2    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
 3        ans = []
-4
-5        def backtrack(state):
-6            if (state not in ans) and (len(state) == len(nums)):
-7                ans.append(state[:])
-8            
-9            for i in range(len(nums)):
-10                if nums[i] in state:
-11                    continue 
-12                state.append(nums[i])
-13                backtrack(state)
-14                state.pop()
-15
-16        backtrack([])
-17        return ans
+4        state = []
+5        candidates.sort()
+6
+7        def backtrack(start, state, total):
+8            if (total == target) and (state not in ans):
+9                ans.append(state[:])
+10                return
+11            
+12            if total > target:
+13                return
+14            
+15            for i in range(start, len(candidates)):
+16                if i != start and candidates[i] == candidates[i-1]:
+17                    continue
+18                state.append(candidates[i])
+19                backtrack(i+1, state, total + candidates[i])
+20                state.pop()
+21        
+22        backtrack(0, [], 0)
+23        return ans
+24        
