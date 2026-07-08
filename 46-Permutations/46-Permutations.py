34fe12ccb3fef263a17fee4f4a2ba5ee4a9eee40
DevAmbani <147029224+DevAmbani@users.noreply.github.com>
2026-07-08 15:47:57 -0500

    File updated at 7/8/2026, 3:47:56 PM

--- 46-Permutations/46-Permutations.py
+++ 46-Permutations/46-Permutations.py
@@ -1,4 +1,4 @@
-# Last updated: 7/8/2026, 3:47:22 PM
+# Last updated: 7/8/2026, 3:47:56 PM
 1class Solution:
 2    def permute(self, nums: List[int]) -> List[List[int]]:
 3        ans = []
@@ -7,13 +7,14 @@
 6        def backtrack(state):
 7            if (state not in ans) and (len(state) == len(nums)):
 8                ans.append(state[:])
-9            
-10            for i in range(len(nums)):
-11                if nums[i] in state:
-12                    continue 
-13                state.append(nums[i])
-14                backtrack(state)
-15                state.pop()
-16
-17        backtrack([])
-18        return ans
+9                return
+10            
+11            for i in range(len(nums)):
+12                if nums[i] in state:
+13                    continue 
+14                state.append(nums[i])
+15                backtrack(state)
+16                state.pop()
+17
+18        backtrack([])
+19        return ans
