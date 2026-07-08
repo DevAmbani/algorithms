5470e02d0b34605e6f9ae51fda14597357bf732b
DevAmbani <147029224+DevAmbani@users.noreply.github.com>
2026-07-08 15:47:22 -0500

    Create 46-Permutations

---
+++ 46-Permutations/46-Permutations.py
@@ -0,0 +1,19 @@
+# Last updated: 7/8/2026, 3:47:22 PM
+1class Solution:
+2    def permute(self, nums: List[int]) -> List[List[int]]:
+3        ans = []
+4        current = []
+5
+6        def backtrack(state):
+7            if (state not in ans) and (len(state) == len(nums)):
+8                ans.append(state[:])
+9            
+10            for i in range(len(nums)):
+11                if nums[i] in state:
+12                    continue 
+13                state.append(nums[i])
+14                backtrack(state)
+15                state.pop()
+16
+17        backtrack([])
+18        return ans
