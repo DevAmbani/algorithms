fa33d88cc720dc49195533243b0f2f1a8ff4a106
DevAmbani <147029224+DevAmbani@users.noreply.github.com>
2026-07-10 10:28:33 -0500

    File updated at 7/10/2026, 10:28:33 AM

--- 46-Permutations/46-Permutations.py
+++ 46-Permutations/46-Permutations.py
@@ -1,4 +1,4 @@
-# Last updated: 7/8/2026, 6:42:54 PM
+# Last updated: 7/10/2026, 10:28:33 AM
 1class Solution:
 2    def exist(self, board: List[List[str]], word: str) -> bool:
 3        if not word:
@@ -14,27 +14,26 @@
 13            if row < 0 or row >= len(board):
 14                return False
 15            if col < 0 or col >= len(board[0]):
-16                return False
-17            
-18            if (row, col) in visited:
-19                return False
-20
-21            if word[index] != board[row][col]:
-22                return False
-23            
-24            visited.add((row, col))
-25            
-26            found = dfs(row+1, col, index+1) or dfs(row-1, col, index+1) or dfs(row, col+1, index+1) or dfs(row, col-1, index+1)
-27
-28            visited.remove((row, col))
-29
-30            return found
-31
-32        for row in range(len(board)):
-33            for col in range(len(board[0])):
-34                if board[row][col] == word[0]:
-35                    if dfs(row, col, 0):
-36                        return True
-37        
-38        return False
-39              
+16                return False     
+17            if (row, col) in visited:
+18                return False
+19
+20            if word[index] != board[row][col]:
+21                return False
+22            
+23            visited.add((row, col))
+24            
+25            found = dfs(row+1, col, index+1) or dfs(row-1, col, index+1) or dfs(row, col+1, index+1) or dfs(row, col-1, index+1)
+26
+27            visited.remove((row, col))
+28
+29            return found
+30
+31        for row in range(len(board)):
+32            for col in range(len(board[0])):
+33                if board[row][col] == word[0]:
+34                    if dfs(row, col, 0):
+35                        return True
+36        
+37        return False
+38              
