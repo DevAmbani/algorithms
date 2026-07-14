4e27fdd48e09de7b8f29da9d25c3fefcba022eaf
DevAmbani <147029224+DevAmbani@users.noreply.github.com>
2026-07-14 17:16:12 -0500

    File updated at 7/14/2026, 5:16:12 PM

--- 133-CloneGraph/133-CloneGraph.py
+++ 133-CloneGraph/133-CloneGraph.py
@@ -1,29 +1,15 @@
-# Last updated: 7/13/2026, 4:35:45 PM
-1"""
-2# Definition for a Node.
-3class Node:
-4    def __init__(self, val = 0, neighbors = None):
-5        self.val = val
-6        self.neighbors = neighbors if neighbors is not None else []
-7"""
-8
-9from typing import Optional
-10class Solution:
-11    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-12        cloned = {}
-13
-14        def dfs(node):
-15            if node in cloned:
-16                return cloned[node]
-17            
-18            clone = Node(node.val)
-19            cloned[node] = clone
-20            
-21            for neighbor in node.neighbors:
-22                soln = dfs(neighbor)
-23                clone.neighbors.append(soln)
-24            
-25            return clone
-26        
-27        return dfs(node) if node else None
-28        
+# Last updated: 7/14/2026, 5:16:12 PM
+1class Solution:
+2    def climbStairs(self, n: int) -> int:
+3        if n <= 2:
+4            return n
+5        
+6        dp = [0] * (n+1)
+7        dp[1] = 1
+8        dp[2] = 2
+9
+10        for i in range(3, n+1):
+11            dp[i] = dp[i-1] + dp[i-2]
+12        
+13        return dp[n]
+14
