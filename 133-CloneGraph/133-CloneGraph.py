b0d89a220348f2190c5e14249514aea3c9f0f04d
DevAmbani <147029224+DevAmbani@users.noreply.github.com>
2026-07-17 12:45:46 -0500

    File updated at 7/17/2026, 12:45:46 PM

--- 133-CloneGraph/133-CloneGraph.py
+++ 133-CloneGraph/133-CloneGraph.py
@@ -1,15 +1,21 @@
-# Last updated: 7/14/2026, 5:17:29 PM
+# Last updated: 7/17/2026, 12:45:46 PM
 1class Solution:
-2    def climbStairs(self, n: int) -> int:
-3        if n <= 2:
-4            return n
-5        
-6        dp = [0] * (n+1)
-7        dp[1] = 1
-8        dp[2] = 2
+2    def rob(self, nums: List[int]) -> int:
+3        if len(nums) == 0:
+4            return 0
+5        if len(nums) == 1:
+6            return nums[0]
+7        if len(nums) == 2:
+8            return max(nums[0], nums[1])
 9
-10        for i in range(3, n+1):
-11            dp[i] = dp[i-1] + dp[i-2]
-12        
-13        return dp[n]
+10        def dp(nums):
+11            dp = [0] * len(nums)
+12            dp[0] = nums[0]
+13            dp[1] = max(nums[0], nums[1])
 14
+15            for i in range(2, len(nums)):
+16                dp[i] = max(dp[i-1], dp[i-2]+nums[i])
+17            
+18            return dp[len(nums)-1]
+19            
+20        return max(dp(nums[1:]),dp(nums[:-1]))
