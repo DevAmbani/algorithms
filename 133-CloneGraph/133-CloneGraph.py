0f767d085be1fcbf5224ae1a6a7ae2eeda57a6a3
DevAmbani <147029224+DevAmbani@users.noreply.github.com>
2026-07-21 17:39:44 -0500

    File updated at 7/21/2026, 5:39:44 PM

--- 133-CloneGraph/133-CloneGraph.py
+++ 133-CloneGraph/133-CloneGraph.py
@@ -1,24 +1,16 @@
-# Last updated: 7/21/2026, 5:32:33 PM
+# Last updated: 7/21/2026, 5:39:44 PM
 1class Solution:
-2    def longestPalindrome(self, s: str) -> str:
-3        longest = ""
+2    def countSubstrings(self, s: str) -> int:
+3        self.count = 0
 4
 5        def expand(l, r):
 6            while l >= 0 and r < len(s) and s[l] == s[r]:
 7                l -= 1
-8                r+= 1
-9            
-10            return s[l+1:r]
-11        
-12        for i in range(len(s)):
-13            even = expand(i, i)
-14            odd = expand(i, i+1)
-15
-16            if len(even) > len(longest):
-17                longest = even
-18            if len(odd) > len(longest):
-19                longest = odd
-20        
-21        return longest
-22
-23
+8                r += 1
+9                self.count += 1
+10        
+11        for i in range(len(s)):
+12            even = expand(i, i)
+13            odd = expand(i, i+1)
+14
+15        return self.count
